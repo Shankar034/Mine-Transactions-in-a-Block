0609e2897d1982e122ed4e3a1154380560c36303
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2022-09-06 23:14:30 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const ChainUtil = require('../chain-util');
+const Transaction = require('./transactions');
+const { INITIAL_BALANCE } = require('../config');
+
+
+class Wallet {
+    constructor(){
+        this.balance = INITIAL_BALANCE;
+        this.keyPair = ChainUtil.genKeyPair;
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+
+    toString(){
+        return `Wallet -
+        publicKey: ${this.publicKey.toString()}
+        balance : ${this.balance}`
+    }
+
+    sign(dataHash){
+        return this.keyPair.sign(dataHash);
+    }
+
+    createTransaction(recipient, amount, TransactionPool){
+        if(amount > this.balance){
+            console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
+            return;
+        }
+
+        let transaction = TransactionPool.existingTransaction(this.publicKey);
+
+        if(transaction){
+            transaction.update(this, recipient, amount);
+        }else{
+            transaction= Transaction.newTransaction(this, recipient, amount);
+            TransactionPool.updateOrAddTransaction(transaction);
+        }
+        return transaction;
+
+    }
+}
+
+module.exports = Wallet;
+
+//getPublic()
